3a5e5113fc4051ca4cfde90754127bf87a6df3b2
Devin Osorio <me@devinosor.io>
2017-05-11 11:55:18 -0700

    refactors app, server files

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import express from 'express';
-import routes from './routes/index';
+import eventController from './controllers/event';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
 const app = express();
+const namespace = '/api/v1';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json'})); 
 
 if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('dev'));
 }
 
-app.use('/api/v1', routes);
+app.use(`${namespace}/events`, eventController);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import app from './app';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+export default app.listen(port, () => {
   if (process.env.NODE_ENV !== 'test') {
     // eslint-disable-next-line no-console
     console.log(`Listening at http://localhost:${port}`);
