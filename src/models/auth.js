6958c6565855eb47b503e580e207c80ac4b485dd
Devin Osorio <me@devinosor.io>
2017-05-14 10:30:16 -0700

    refactors auth model

--- src/models/auth.js
+++ src/models/auth.js
@@ -1,6 +1,21 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import knex from './knex';
 
 export const authenticate = (body) => {
   return knex('users')
-  .where('username', body.username);
+  .where('username', body.username)
+  .then((user) => {
+    return bcrypt.compare(body.password, user[0].password)
+    .then((response) => {
+      if (response) {
+        const payload = { username: user[0].username };
+        const expires = { expiresIn: '24h' };
+
+        return jwt.sign(payload, process.env.SECRET, expires);
+      } else {
+        return 'password is not valid';
+      }
+    });
+  });
 };
