57d1956d26ecb3fe5b54861a11d1c2f081ab2d09
Devin Osorio <me@devinosor.io>
2017-05-14 10:31:52 -0700

    adds feature to add space after coma in error messages

--- src/app.js
+++ src/app.js
@@ -25,14 +25,12 @@ app.use((req, res, next) => {
   next(err);
 });
 
-if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    success: false,
+    message: err.message.split(',').join(', '),
   });
-}
+});
 
 export default app;
